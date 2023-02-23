7e20f7d5f4162b1891289f7c0f38e9d655164d73
Zagz01 <youripinzani@hotmail.com>
2023-02-23 21:36:27 +0100

    deleted an unecessary title

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
 
   return (
           <div className="App">
-            <h1>Rock Paper Scissors</h1>
             <div>
             {
               !againstPlayer && !againstComputer ? (
