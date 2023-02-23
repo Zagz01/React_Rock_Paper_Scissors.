46f0c881352ec1d75d1dee29720667184b133f2e
Zagz01 <youripinzani@hotmail.com>
2023-02-24 00:05:29 +0100

    changed the result message for both modes player and vs computer for a better visual aspect

--- src/component/GamevsPlayer/GamevsPlayer.js
+++ src/component/GamevsPlayer/GamevsPlayer.js
@@ -38,9 +38,9 @@ useEffect(() => {
             || (playerChoice === 'Paper' && player2Choice === 'Rock') 
             || (playerChoice === 'Scissors' && player2Choice === 'Paper')
         ) {
-            setResult(`player won with ${playerChoice} and player 2 lost with ${player2Choice}`);
+            setResult(`The player won with ${playerChoice} and player 2 lost with ${player2Choice}`);
         } else {
-            setResult(`player 2 won with ${player2Choice} and player lost with ${playerChoice}`);
+            setResult(`The player 2 won with ${player2Choice} and player lost with ${playerChoice}`);
         }
     }
 }, [playerChoice, player2Choice]);
